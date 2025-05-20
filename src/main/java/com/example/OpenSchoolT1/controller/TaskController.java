577539a65803eae7cf62c19f364c734df0dc2208
Madinusha <madinamalika19@yandex.ru>
2025-05-20 16:30:29 +0300

    Перенесено логирование в отдельный стартер.

--- src/main/java/com/example/OpenSchoolT1/controller/TaskController.java
+++ src/main/java/com/example/OpenSchoolT1/controller/TaskController.java
@@ -6,6 +6,7 @@ import com.example.OpenSchoolT1.service.TaskService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import com.example.OpenSchoolT1Starter.annotation.HttpLog;
 
 import java.util.List;
 
@@ -18,28 +19,33 @@ public class TaskController {
 		this.taskService = taskService;
 	}
 
+	@HttpLog
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public TaskResponseDTO createTask(@RequestBody @Valid TaskRequestDTO dto) {
 		return taskService.createTask(dto);
 	}
 
+	@HttpLog
 	@GetMapping("/{id}")
 	public TaskResponseDTO getTaskById(@PathVariable Long id) {
 		return taskService.getTaskById(id);
 	}
 
+	@HttpLog
 	@PutMapping("/{id}")
 	public TaskResponseDTO updateTask(@PathVariable Long id, @RequestBody @Valid TaskRequestDTO dto) {
 		return taskService.updateTask(id, dto);
 	}
 
+	@HttpLog
 	@DeleteMapping("/{id}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void deleteTask(@PathVariable Long id) {
 		taskService.deleteTask(id);
 	}
 
+	@HttpLog
 	@GetMapping
 	public List<TaskResponseDTO> getAllTasks() {
 		return taskService.getAllTasks();
